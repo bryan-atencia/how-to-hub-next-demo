880748a7e8d4c9307ca68a8f868958b3293efd29
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-25 17:12:39 -0500

    work

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,26 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout.js'
+import { useEffect } from "react"
 
-useEffect(()=>{
-  if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", user => {
-      if (!user) {
-        window.netlifyIdentity.on("login", () => {
-          document.location.href = "/admin/";
-        });
-      }
-    });
-  }
-},[])
 
-export default () => (
-        <Layout>
-          <Head>
-            <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-          </Head>
-          <h1>Home</h1>
-          <Link href="category"><a>to Category</a></Link>
-        </Layout>
-)
+export default () => {
+  useEffect(()=>{
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  },[])
+
+  return <Layout>
+            <Head>
+              <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+            </Head>
+            <div>Home</div>
+          </Layout>
+}

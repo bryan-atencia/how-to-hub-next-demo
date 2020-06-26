c797956b5dcb18feb018eb540b89772a352a4af1
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-26 09:47:36 -0500

    adjust

--- functions/getHows.js
+++ functions/getHows.js
@@ -3,7 +3,7 @@ let path = require("path")
 let { join } = require("path")
 
 export default () => {
-  let filePath = "./public/admin/collections/How-tos"
+  let filePath = "../public/admin/collections/How-tos"
   let folder = fs.readdirSync(filePath).map(name => join(filePath, name))
   let data = folder.map(x => JSON.parse(fs.readFileSync(x, 'utf-8')))
   return data

868f7b9a45a1bcb07b6bbeda7afed747a025747b
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-26 17:04:00 -0500

    update config.yml

--- pages/index.js
+++ pages/index.js
@@ -1,76 +1,44 @@
 import { useEffect } from "react"
-import  getCategories from '../public/admin/functions/getCategories.js'
 
 import Head from 'next/head'
 import Link from 'next/link'
-
 import Layout from '../components/layout.js'
 
-import { Grid, Typography, makeStyles } from "@material-ui/core"
-
-const useStyles = makeStyles({
-  mainGrid: {
-    maxWidth:"775px",
-    margin:"50px 0"
-  },
-  image: {
-    background:"grey",
-    height:"250px",
-    width:"250px",
-    borderRadius:"50%",
-    backgroundColor:"#ffe5b4"
-  },
-  actionText: {
-    textTransform:"uppercase",
-    textAlign:"center",
-    borderBottom:"1px solid",
-    width:"fit-content",
-    margin:"10px auto",
-    color:"black"
-  },
-  actionLink: {
-    color:"black",
-    textDecoration:"none"
-  }
-});
+import getHows from '../public/admin/functions/getHows.js'
 
 export default (props) => {
 
-  const classes = useStyles();
-
-  const renderCategories = () => {
-
-    const { data } = props
-
-    return <Grid container justify="space-between" alignItems="center">
-            {
-                data && data.map((x, y) => {
-                  return <Grid item key={ y }>
-                            <Grid className={ classes.image }></Grid>
-                              <Link href={`/category/${x.name.split(" ").join("")}`}>
-                                <Typography className={ classes.actionText }>{ x.name }</Typography>
-                              </Link>
-                            </Grid>
-                })
-            }
-          </Grid>
-  }
+  useEffect(()=>{
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  },[])
 
   return <Layout>
-            <Grid className={ classes.mainGrid }>
-              <Typography variant="h2" gutterBottom>The Bartender</Typography>
-              <Typography variant="h6">Your complete guide to men’s apparel and accessories. Browse style tips, size guides, and steps to master tying a necktie, tying a bow tie, folding a pocket square, and more.</Typography>
-            </Grid>
-              <Grid>
-                { renderCategories() }
-              </Grid>
+            <h1>Home</h1>
+              <div>
+                {
+                  props.data && props.data.map((x, y) => {
+                    return <Link href="/category" key={ y }>
+                              <a style={{ display:"block", margin:"10px 0" }}>{ x.name }</a>
+                            </Link>
+                  })
+                }
+              </div>
           </Layout>
 }
 
 export async function getStaticProps() {
   return {
     props: {
-      data: getCategories()
+      fallback: true,
+      data: getHows()
     }
   }
 }

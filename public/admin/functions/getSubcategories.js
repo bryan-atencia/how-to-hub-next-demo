43309204b0520828207a67ddc28cb4340bf6084d
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-29 08:29:42 -0500

    change 2

--- public/admin/functions/getSubcategories.js
+++ public/admin/functions/getSubcategories.js
@@ -2,7 +2,7 @@ import fs from "fs"
 import { path, join } from "path"
 
 export default () => {
-  let filePath = "./public/admin/collections/Subategories"
+  let filePath = "./public/admin/collections/Subcategories"
   let folder = fs.readdirSync(filePath).map(name => join(filePath, name))
   let data = folder.map(x => JSON.parse(fs.readFileSync(x, 'utf-8')))
   return data

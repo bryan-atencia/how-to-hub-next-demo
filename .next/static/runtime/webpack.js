eb21a7b842b2161b52812a8e1dc2851d6bb63140
bryan-atencia <bryan.atencia@thetiebar.com>
2020-06-29 09:41:21 -0500

    fix config

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "14680324efc6cf2770c6";
+/******/ 	var hotCurrentHash = "9d1f5d4949098bad07bf";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
